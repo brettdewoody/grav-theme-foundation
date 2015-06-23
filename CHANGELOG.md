fa98d53e91d643dd8b6e98b8ca123a114d7aad0a
brettdewoody <brett@digitalwaxworks.com>
2015-06-22 23:05:59 -0600

    Update.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v1.0.0
+## 6/22/2015
+
+1. [](#new)
+    * ChangeLog Started
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+gulp.task('sass', function () {
+  gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
