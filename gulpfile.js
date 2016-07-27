bb8b542ad2110729680c55be89fc2060c44130cb
Max Hilland <max.hilland@gmail.com>
2016-07-27 07:22:04 -0700

    added bower components, build improvements

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,18 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 var watch = require('gulp-watch');
+
+gulp.task('default', ['sass', 'watch']);
 
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(concat('app.css'))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
 });
